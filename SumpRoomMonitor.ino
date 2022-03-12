1a2b0905c50ad72be470777fe8a7235b13782b62
Scott W. Vincent <scott@swvincent.com>
2022-03-12 09:53:05 -0500

    Only notify on state change, indicate Sump okay change

--- SumpRoomMonitor.ino
+++ SumpRoomMonitor.ino
@@ -7,6 +7,9 @@
  * 
  * WiFi Setup based on:
  * https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
+ * 
+ * Also helpful:
+ * https://www.arduino.cc/en/Tutorial/BuiltInExamples/Debounce
  */
 
 #include <ESP8266WiFi.h>
@@ -14,6 +17,12 @@
 
 // I/O Settings
 const int ALARM_RELAY = 9;
+
+// Logic
+const unsigned long MIN_ALARM_TIME = 5000;
+unsigned long alarmStateChangeMillis;
+int alarmState = LOW;
+int lastAlarmState = LOW;
 
 // WiFi Settings
 const char* WIFI_SSID = "SSID";
@@ -61,25 +70,54 @@ void setupWiFi()
 // Main Loop
 void loop() 
 {
-
-  // Alarm is active when low
-  if (digitalRead(ALARM_RELAY) == LOW) 
-  {
-    // Debounce
-    delay(100);
-    if ((digitalRead(ALARM_RELAY) == LOW)) 
-    {
-      NotifySumpAlarmActive();
-    }
-  }
+  ProcessAlarmState();
 }
 
-void NotifySumpAlarmActive()
+void ProcessAlarmState()
 {
-  Serial.println("Sump Pump Alarm Active");
-  pushover_button.setTitle("Sump Pump Alarm Active");
-  pushover_button.setMessage("The backup sump pump controller is in an alarm condition.");
+  int reading = digitalRead(ALARM_RELAY);
+
+  if (reading != lastAlarmState)
+  {
+    alarmStateChangeMillis = millis();
+  }
+  
+  if ((millis() - alarmStateChangeMillis) > MIN_ALARM_TIME)
+  {
+    if (alarmState != reading)
+    {
+      alarmState = reading;
+
+      if (alarmState == HIGH)
+      {
+        NotifySumpAlarm();
+      }
+      else
+      {
+        NotifySumpOkay();
+      }
+    }
+  }
+
+  lastAlarmState = reading;
+}
+
+void NotifySumpAlarm()
+{
+  Serial.println("Sump Pump Alarm");
+  pushover_button.setTitle("Sump Pump Alarm");
+  pushover_button.setMessage("The sump pump controller is in an alarm condition.");
   pushover_button.setSound("vibrate");
   pushover_button.setPriority(1);
+  Serial.println(pushover_button.send());
+}
+
+void NotifySumpOkay()
+{
+  Serial.println("Sump Pump Okay");
+  pushover_button.setTitle("Sump Pump Okay");
+  pushover_button.setMessage("The sump pump controller is okay.");
+  pushover_button.setSound("vibrate");
+  pushover_button.setPriority(0);
   Serial.println(pushover_button.send());
 }
